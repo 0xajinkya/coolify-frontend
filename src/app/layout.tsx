cacbfe1f9de27a0a640711d3ee95b8392f90c910
Ajinkya Anil Kahane <kahaneajinkya051@gmail.com>
2024-07-08 10:10:49 +0530

    feat: anallytics and speed insights added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Box } from "@mui/material";
 import { AllProviders } from "@/components";
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://coolify.top"),
